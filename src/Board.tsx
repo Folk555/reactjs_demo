e02c13feb510ab0ec704480dbf765a6b231153f1
aturulin <Aleksandr.Turulin@kauri-iot.com>
2023-07-07 17:51:14 +0300

    изменена иерархия функций чтоб поле могло наблюдать за статусом всех кнопок(квадратов)

--- src/Board.tsx
+++ src/Board.tsx
@@ -4,38 +4,46 @@ import { useState } from "react";
 
 export default function Board() {
 
-    function Button( {value}: {value?: string}) {
-        return <button
-        className="square" 
-        onClick={handleClick}
-        >
-            {value}
-        </button>
-    }
-    
-    const [state, setState] = useState(null)
-    
-    function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
-        console.log("Clicked")
+    const [squares, setSquare] = useState(Array<string|null>(9).fill(null))
+    function handleClick(i: number) {
+        const nextSquares = squares.slice()
+        nextSquares[i] = "X"
+        setSquare(nextSquares)
     }
 
   return (
     <div className="board">
         <div>
-            <Button />
-            <Button />
-            <Button />
+            <Button value={squares[0]} onSquareClick={() => handleClick(0)}/>
+            <Button value={squares[1]} onSquareClick={() => handleClick(1)}/>
+            <Button value={squares[2]} onSquareClick={() => handleClick(2)}/>
         </div>
         <div>
-            <Button />
-            <Button />
-            <Button />
+            <Button value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Button value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Button value={squares[5]} onSquareClick={() => handleClick(5)}/>
         </div>
         <div>
-            <Button />
-            <Button />
-            <Button />
+            <Button value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Button value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Button value={squares[8]} onSquareClick={() => handleClick(8)}/>
         </div>
     </div>
   );
 }
+
+interface IButtonValue {
+    value: string | null,
+    onSquareClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+function Button(input: IButtonValue) {
+
+    return <button
+    className="square" 
+    onClick={input.onSquareClick}
+    >
+        {input.value}
+    </button>
+}
+
