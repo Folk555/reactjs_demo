ee7db8c3ee0dadf16d9d1e1857cf6ef0fd39937c
aturulin <Aleksandr.Turulin@kauri-iot.com>
2023-07-10 14:48:30 +0300

    реализован возврат по истории игры

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,18 +7,40 @@ export default function Game() {
 
     const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array<string|null>(9).fill(null)])
-    const currentSquares = history[history.length - 1]
-
+    const [currentMove, setCurrentMove] = useState(0);
+    const currentSquares = history[currentMove]
+    
     function handlePlay(nextSquares: (string|null)[]) {
-        setHistory([...history, nextSquares])
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
         setXIsNext(!xIsNext)
       }
+
+    function jumpTo(nextMove: number) {
+        setCurrentMove(nextMove);
+        setXIsNext(nextMove % 2 === 0);
+    }
+
+    const moves = history.map((squares, move) => {
+        let description;
+        if (move > 0) {
+          description = 'Go to turn #' + move;
+        } else {
+          description = 'Go to game start';
+        }
+        return (
+          <li key={move}>
+            <button className="history" onClick={() => jumpTo(move)}>{description}</button>
+          </li>
+        );
+      });
 
     return (
         <>
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-        <div>
-            <ol>OOOOOOOOOOLLLLLLLLLL</ol>
+        <div className="history">
+            <ol>{moves}</ol>
         </div>
         </>
   );
